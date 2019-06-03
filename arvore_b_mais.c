095068b6c470668cc60e57b46db92699535ab0c1
Daniel Lessa <daniellsf@id.uff.br>
2019-06-03 00:32:35 +0000

    Implementando funcao de busca na arvore b+ e resolvendo teste do 1 ao 5

--- arvore_b_mais.c
+++ arvore_b_mais.c
@@ -6,11 +6,34 @@
 
 #include "arvore_b_mais.h"
 #include "lista_pizzas.h"
-
+#include "metadados.h"
+#include "no_folha.h"
+#include "no_interno.h"
+#include "pizza.h"
 
 int busca(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados, int d)
 {
-	//TODO: Inserir aqui o codigo do algoritmo
+	TMetadados* meta = le_arq_metadados(nome_arquivo_metadados);
+    
+    // >= - ESQUERDA e < - DIREITA
+    if(meta->raiz_folha) return meta->pont_raiz;
+    else{
+        int folha = -1;
+        int no_atual = meta->pont_raiz;
+        FILE* arq_indices = fopen(nome_arquivo_indice, "rb");
+        
+        while(folha == -1){
+            TNoInterno* pagina = le_no_interno(d, arq_indices);
+            int i;
+            for(i = 0; cod >= pagina->chaves[i] && i <= pagina->m; i++);
+            if(pagina->aponta_folha) folha = pagina->p[i];
+            else no_atual = pagina->p[i];
+        }
+
+        fclose(arq_indices);
+        return folha;
+    }
+
     return INT_MAX;
 }
 
